a2830e00d5517c0dc391b9e585b33de4fc886efe
Viet Pham <tug36669@temple.edu>
2020-02-14 14:41:22 -0500

    Added new activity for chat window

--- app/src/main/java/com/example/mapchatapp/ChatWindow.java
+++ app/src/main/java/com/example/mapchatapp/ChatWindow.java
@@ -2,7 +2,11 @@ package com.example.mapchatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ChatWindow extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class ChatWindow extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_window);
+
+        Intent chatIntent = getIntent();
+        String username = chatIntent.getStringExtra("username");
+
+        TextView userNameTextView = findViewById(R.id.userNameTextView);
+        userNameTextView.setText(username);
     }
 }
